e16fe0fe2585ac1df1b41eb77f7a68405b1f3df7
PopovaOlha <fc.odess.10@gmail.com>
2024-10-11 01:58:21 +0300

    fix: correct current directory output with cwd() call

--- src/utils/showCurrentDirectoryMessage.js
+++ src/utils/showCurrentDirectoryMessage.js
@@ -1,7 +1,7 @@
 import { cwd } from 'node:process'
 
 const showCurrentDirectoryMessage = () => {
-	console.info(`You are currently in ${cwd}`)
+	console.info(`You are currently in ${cwd()}`)
 }
 
 export default showCurrentDirectoryMessage
