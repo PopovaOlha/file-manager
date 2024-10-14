e063483a7197978a6c9c21ce0ff4d836f8287c50
PopovaOlha <fc.odess.10@gmail.com>
2024-10-15 00:36:50 +0300

    feat: refactored chooseCommand function to handle various file operations

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,10 +1,21 @@
-import { argv } from 'node:process'
+import { argv, stdin, stdout } from 'node:process'
+import * as readline from 'readline/promises'
+import chooseCommand from './utils/chooseCommand.js'
 import showCurrentDirectoryMessage from './utils/showCurrentDirectoryMessage.js'
 
 const args = argv.slice(2)
 const argName = String(args).split('=')[1]
 const userName = argName !== 'your_username' ? argName : 'Anonymous'
 
+const rl = readline.createInterface({ input: stdin, output: stdout })
+
 console.info(`Welcome to the File Manager, ${userName}!`)
 
 showCurrentDirectoryMessage()
+
+rl.on('line', async data => chooseCommand(data, userName))
+	.on('SIGINT', () => rl.close())
+	.on('close', () =>
+		console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
+	)
+	.on('error', () => console.error('Invalid input'))
